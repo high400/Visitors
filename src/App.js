90181db8d4dd173b8157ab6dedbf07e9c0f26f9f
Hang <sapbatcheung@gmail.com>
2021-07-17 17:22:39 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-
+import "./index.css";
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-        <a
-          
-        >
-          Tarantino, go suck a dick 
-        </a>
-      </header>
+      
+      <button>Delete</button>
     </div>
   );
 }
